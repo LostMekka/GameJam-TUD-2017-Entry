195c2c7b6e015f5f3abae7ea5c63f5a1833e1430
LostMekka <lostmekka@web.de>
2017-10-27 21:52:24 +0200

    added map script

---
+++ Assets/scripts/Map.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour
+{
+	public int GeneratedSize = 10;
+	public float TileDistance = 1f;
+	public GameObject TilePrefab;
+
+	// Use this for initialization
+	void Start()
+	{
+		GenerateNewMap();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+	}
+
+	public void GenerateNewMap()
+	{
+		for (int y = 0; y < GeneratedSize; y++)
+		{
+			for (int x = 0; x < GeneratedSize; x++)
+			{
+				GameObject tile = Instantiate(TilePrefab);
+				tile.transform.position = GetTilePosition(x, y);
+			}
+		}
+	}
+
+	private Vector3 GetTilePosition(int x, int y)
+	{
+		return new Vector3(
+			x * TileDistance + (y % 2) * TileDistance / 2f,
+			0,
+			(float) Math.Sqrt(3) / 2f * TileDistance * y
+		);
+	}
+}
