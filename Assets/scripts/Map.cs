841a9b79d5ca2b504ed811ebc75b37d610d1c5fc
LostMekka <lostmekka@web.de>
2017-10-27 23:08:39 +0200

    added walls to generation

--- Assets/scripts/Map.cs
+++ Assets/scripts/Map.cs
@@ -6,11 +6,13 @@ public class Map : MonoBehaviour
 {
 	public int GeneratedSize = 10;
 	public float TileDistance = 1f;
-	public GameObject TilePrefab;
+	public float WallProbability = 0.1f;
+	public GameObject FloorTilePrefab;
+	public GameObject WallTilePrefab;
 
 	private Dictionary<int, GameObject> tiles = new Dictionary<int, GameObject>();
 	private const int MaxMapSize = 9999;
-	
+
 	void Start()
 	{
 		GenerateNewMap();
@@ -21,14 +23,17 @@ public class Map : MonoBehaviour
 		get { return tiles[x + y * MaxMapSize]; }
 		set { tiles[x + y * MaxMapSize] = value; }
 	}
-	
+
 	public void GenerateNewMap()
 	{
 		for (int y = 0; y < GeneratedSize; y++)
 		{
 			for (int x = 0; x < GeneratedSize; x++)
 			{
-				GameObject tile = Instantiate(TilePrefab);
+				GameObject prefab = UnityEngine.Random.value < WallProbability
+					? WallTilePrefab
+					: FloorTilePrefab;
+				GameObject tile = Instantiate(prefab);
 				tile.transform.position = GetTilePosition(x, y);
 				this[x, y] = tile;
 			}
