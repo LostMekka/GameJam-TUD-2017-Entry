ebf2edc31cd5e8e5041cc72949d75fdecc17bbb9
Steve <masterredstorm@gmail.com>
2017-10-28 18:41:17 +0200

    Added new class to Chracter.cs

--- Assets/scripts/Character.cs
+++ Assets/scripts/Character.cs
@@ -23,9 +23,14 @@ public class Character : MonoBehaviour
 	public string CurrentActionName { get { return CurrentAction.Name; } }
 	public bool IsDead { get { return Health <= 0; } }
 
+    //is true if Animation is playing
+    public bool inAnimation = false;
 
-	// Use this for initialization
-	void Start()
+    private Animator animator;
+
+
+    // Use this for initialization
+    void Start()
 	{
 		model = Instantiate(ModelPrefab);
 		model.transform.localScale = new Vector3(ModelScale, ModelScale, ModelScale);
@@ -33,10 +38,14 @@ public class Character : MonoBehaviour
 
 		Health = MaxHealth;
 		Stamina = MaxStamina;
-	}
 
-	// Update is called once per frame
-	void Update() { }
+        //gets Animator component if it is on the same GameObject
+        //if it is somewhere else this needs to be changed:
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update() { }
 
 	public ActionAtom ExecuteTurn()
 	{
@@ -55,4 +64,63 @@ public class Character : MonoBehaviour
 		tile.CharacterStandingThere = this;
 		OccupiedTile = tile;
 	}
+
+    //returns true if animation is done
+    private void StartAnimation() {
+        if (CurrentAction.CurrentTurnActionAtom.Type == ActionType.Idle)
+        {
+            animator.SetTrigger("unitIdle");
+        }
+        else if (CurrentAction.CurrentTurnActionAtom.Type == ActionType.Move)
+        {
+            animator.SetTrigger("unitIdle");
+        }
+        else if (CurrentAction.CurrentTurnActionAtom.Type == ActionType.Roll)
+            //doubled animations
+        {
+            animator.SetTrigger("unitIdle");
+
+        }
+        else if (CurrentAction.CurrentTurnActionAtom.Type == ActionType.Block)
+            //doubled animations
+        {
+            animator.SetTrigger("unitIdle");
+
+        }
+        else if (CurrentAction.CurrentTurnActionAtom.Type == ActionType.Buildup)
+        {
+            //doubled animations
+            animator.SetTrigger("unitIdle");
+        }
+        else if (CurrentAction.CurrentTurnActionAtom.Type == ActionType.Recover)
+        {
+            //doubled animations
+            animator.SetTrigger("unitIdle");
+        }
+        else if (CurrentAction.CurrentTurnActionAtom.Type == ActionType.AttackSingleTile)
+        {
+            animator.SetTrigger("unitIdle");
+        }
+        else if (CurrentAction.CurrentTurnActionAtom.Type == ActionType.Attack3Tiles)
+        {
+            //doubled animations
+            animator.SetTrigger("unitIdle");
+
+        }
+        else if (CurrentAction.CurrentTurnActionAtom.Type == ActionType.Attack5Tiles)
+        {
+            //doubled animations
+            animator.SetTrigger("unitIdle");
+        }
+        else if (CurrentAction.CurrentTurnActionAtom.Type == ActionType.AttackAllTiles)
+        {
+            //doubled animations
+            animator.SetTrigger("unitIdle");
+        }
+        else if (CurrentAction.CurrentTurnActionAtom.Type == ActionType.Rotate)
+        {
+            //doubled animations
+            animator.SetTrigger("unitIdle");
+        }
+    }
 }
